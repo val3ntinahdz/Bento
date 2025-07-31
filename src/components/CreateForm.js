0b107a925203983ae036663abb9ae5a32de64433
val3ntinahdz <valentinaleonh8@gmail.com>
2025-07-31 14:47:42 -0600

    Create form logic validation (not fixed yet)

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -2,64 +2,117 @@ import '../styles/forms.css';
 import { CreateButtons } from './CreateButtons';
 
 export const CreateForm = () => {
-    const form = document.createElement("form");
+    let form = document.createElement("form");
     form.id = "create-new-client-form";
     form.classList.add("hidden");
 
     const formH1 = document.createElement("h1");
     formH1.innerText = "Create new client";
-
     form.appendChild(formH1);
 
     const formGroup = document.createElement("div");
     formGroup.className = "form-group";
     form.appendChild(formGroup)
 
-    // first label
-    const firstLabel = document.createElement("label");
-    firstLabel.textContent = "Client name";
+    // create inputs!
+    formGroup.appendChild(createFormFields({
+        label: "Client name",
+        type: "text",
+        id: "name",
+        name: "name",
+    }))
 
-    formGroup.appendChild(firstLabel);
+    formGroup.appendChild(createFormFields({
+        label: "Client email",
+        type: "text",
+        id: "email",
+        name: "email",
+    }))
 
-    const myInput = document.createElement('input');
-    myInput.type = "text";
-    myInput.classList.add("input-control");
-    formGroup.appendChild(myInput);
+    formGroup.appendChild(createFormFields({
+        label: "Client phone",
+        type: "text",
+        id: "phone",
+        name: "phone",
+    }))
 
-    // second label
-    const secondLabel = document.createElement("label");
-    secondLabel.textContent = "Client email";
-
-    formGroup.appendChild(secondLabel);
-
-    const secondInput = document.createElement('input');
-    secondInput.type = "text";
-    secondInput.classList.add("input-control");
-    formGroup.appendChild(secondInput);
-
-
-    // third label 
-    const thirdLabel = document.createElement("label");
-    thirdLabel.textContent = "Client phone";
-
-    formGroup.appendChild(thirdLabel);
-
-    const thirdInput = document.createElement('input');
-    thirdInput.type = "text";
-    thirdInput.classList.add("input-control");
-    formGroup.appendChild(thirdInput);
-
+    
     // create a button
     const btnClass = "form-button";
     const btnContent = "Add new client";
     CreateButtons(btnClass, formGroup, btnContent);
-
-
-
+    
     document.body.append(form);
+
+    // validate form inputs after form is created and appended to the DOM
+    setupFormValidation();
     return form;
 }
 
+const createFormFields = ({ label, type, id, name }) => {
+    const inputControl = document.createElement("div");
+    inputControl.className = "input-control";
+
+    const newLabel = document.createElement("label");
+    newLabel.textContent = label;
+
+    const newInput = document.createElement("input");
+    newInput.type = type;
+    newInput.id = id;
+    newInput.name = name;
+    newInput.className = "form-input";
+
+    // create validation class
+    const errorDiv = document.createElement("div");
+    errorDiv.className = "error";
+    // append label and inputs to our div
+    inputControl.append(newLabel, newInput, errorDiv);
+    return inputControl;
+}
+
+
+const setupFormValidation = () => {
+    const form = document.getElementById("create-new-client-form");
+    console.log(form);
+
+    const forminputs = document.querySelectorAll(".form-input");
+    console.log(forminputs);
+
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+
+        forminputs.forEach(input => {
+            const inputValue = input.value.trim();
+            console.log(inputValue);
+
+            if(inputValue === '') {
+                setError(input.id, `${ input.id } is required`);
+            } else {
+                setSuccess(input.id);
+            }
+
+        })
+    })
+
+};
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add("error");
+    inputControl.classList.remove("success");
+}
+
+const setSuccess = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add("success");
+    inputControl.classList.remove("error");
+}
 
 // TASKS PARA MAÑANA
 // Generar id´s dinamicos
